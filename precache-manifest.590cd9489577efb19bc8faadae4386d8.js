3e185e99819a22592958adfd23f5266f4c57c804
Asraf <mohammedasraf567@gmail.com>
2020-09-03 13:23:05 +0530

    Updates

---
+++ precache-manifest.590cd9489577efb19bc8faadae4386d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ca5cd1fdd1b382686494f7d65c9f23",
+    "url": "/Asraf/index.html"
+  },
+  {
+    "revision": "718eb6ee69ee9f8fea4b",
+    "url": "/Asraf/static/css/main.4ff01ac0.chunk.css"
+  },
+  {
+    "revision": "359cd90f5f4e0b3901a3",
+    "url": "/Asraf/static/js/2.ec1138de.chunk.js"
+  },
+  {
+    "revision": "5730d1823f6b27077c677030967812c2",
+    "url": "/Asraf/static/js/2.ec1138de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "718eb6ee69ee9f8fea4b",
+    "url": "/Asraf/static/js/main.113ee130.chunk.js"
+  },
+  {
+    "revision": "1a0e5bfa6bfc645f7b9f",
+    "url": "/Asraf/static/js/runtime-main.6ffb51b3.js"
+  },
+  {
+    "revision": "7dabe10bb7ec339578814e7fd953e6ac",
+    "url": "/Asraf/static/media/covid.7dabe10b.png"
+  },
+  {
+    "revision": "718353260df289e4967a5ec5305b3dae",
+    "url": "/Asraf/static/media/main_picture.71835326.jpg"
+  },
+  {
+    "revision": "1a04a07f2ddc80647d54da702119f128",
+    "url": "/Asraf/static/media/samsung.1a04a07f.jpg"
+  },
+  {
+    "revision": "85138c12903165b01b49f7fb87c8891d",
+    "url": "/Asraf/static/media/shoppingcart.85138c12.jpg"
+  },
+  {
+    "revision": "fe3fd639ce605d67d939c70a4eea9ab2",
+    "url": "/Asraf/static/media/spotify.fe3fd639.jpg"
+  }
+]);
